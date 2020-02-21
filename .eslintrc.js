d173b2a6b3fd438ec740206b93637037c248c05f
Zili Huang <zili.huang@i22.de>
2020-02-21 17:08:09 +0100

    Initial commit and adjust eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,10 @@ module.exports = {
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/no-unused-components": ["warn", {
+      "ignoreWhenBindingPresent": true
+    }]
   },
   overrides: [
     {
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Created by Zili Huang</h1>
+    <PluginList></PluginList>
   </div>
 </template>
+
+<script>
+import PluginList from "@/components/PluginList.vue";
+
+export default {
+  name: "About",
+  components: {
+    PluginList
+  }
+};
+</script>
